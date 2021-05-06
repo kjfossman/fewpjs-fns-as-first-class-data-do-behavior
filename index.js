8eb727319878f6e9f814c18c3add086f33b63278
Kyle <kjfossman@gmail.com>
2021-05-06 09:46:52 -0800

    commit tree 1d6d3950e50c213076f90f2cfd3d506b5f278e9b

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(string){
+  let hour = parseInt(string);
+  if (hour < 12) return "Good Morning";
+  if (hour >= 12 && hour < 17) return "Good Afternoon";
+  if (hour >= 17 && hour < 25) return "Good Evening";
+  return "Not a Valid Time Buddy"
+}
+
+function displayMessage(string){
+  let greeting = document.getElementById("greeting")
+  greeting.innerText = string
+  
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
